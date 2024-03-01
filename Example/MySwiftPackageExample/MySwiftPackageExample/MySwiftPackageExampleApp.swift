fd5643fab8b742c24c0b3c5248bc048ddfc8fc3f
Mukul Joshi <mukulj@dewsolutions.in>
2024-03-01 17:47:09 +0530

    swiftExample added

---
+++ Example/MySwiftPackageExample/MySwiftPackageExample/MySwiftPackageExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  MySwiftPackageExampleApp.swift
+//  MySwiftPackageExample
+//
+//  Created by Mukul Joshi on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct MySwiftPackageExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
