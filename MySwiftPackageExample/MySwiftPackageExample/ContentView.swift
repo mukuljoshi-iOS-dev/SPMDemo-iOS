322b3f9406a906bc65543b7c45513df52c4838d9
Mukul Joshi <mukulj@dewsolutions.in>
2024-03-02 18:23:34 +0530

    example added

--- MySwiftPackageExample/MySwiftPackageExample/ContentView.swift
+++ MySwiftPackageExample/MySwiftPackageExample/ContentView.swift
@@ -6,14 +6,29 @@
 //
 
 import SwiftUI
+import SPM_Demo
 
 struct ContentView: View {
+    
+    let side = 5.0
+    let length = 4.0
+    let width = 6.0
+    let radius = 8.0
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            let square = Shape.square(side: 5.0)
+            Text("Area of square of side \(side): \(square.area())")
+                .padding(30)
+            
+            let rectangle = Shape.rectangle(length: 4.0, width: 6.0)
+            Text("Area of rectangle of \(length) x \(width): \(rectangle.area())")
+                .padding(30)
+
+            let circle = Shape.circle(radius: 3.0)
+            Text("Area of circle of radius \(radius): \(circle.area())")
+            
+            Spacer()
         }
         .padding()
     }
